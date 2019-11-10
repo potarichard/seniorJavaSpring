3ccdb13006faa50c8cd2c5a1b9cf0dc31fbfd70a
myname <richard.i.pota@gmail.com>
2019-11-10 11:23:30 +0100

    start

---
+++ src/main/java/hu/ponte/hr/controller/upload/UploadController.java
@@ -0,0 +1,31 @@
+package hu.ponte.hr.controller.upload;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import hu.ponte.hr.services.ImageStore;
+import hu.ponte.hr.services.SignService;
+@Component
+@RequestMapping("api/file")
+public class UploadController
+{
+	
+	@Autowired
+    private ImageStore imageStore;
+	
+	@Autowired
+	private SignService signService;
+
+    @RequestMapping(value = "post", method = RequestMethod.POST)
+    @ResponseBody
+    public String handleFormUpload(@RequestParam("file") MultipartFile file) {
+    	
+    	    	
+        return "error";
+    }
+}
